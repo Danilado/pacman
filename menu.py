932281d9bbe46237efbd148964c9b2c66eda3b0b
Gospodin <danya.zvyagin@gmail.com>
2021-11-17 19:42:44 +0300

    settings menu adden and updated

--- menu.py
+++ menu.py
@@ -4,71 +4,18 @@ import pygame
 
 import player
 from game import main
+from globalclasses import Button
+from globalclasses import GuiSettings
 from input_name_menu import ask_name
 from perfomance import img_load
 from store_score import store_score
 from store_score_menu import store_score_menu
+from settings_menu import settings_menu
 
 pygame.init()
 pygame.mixer.init()
 
-
-class GuiSettings:
-    def __init__(self):
-        self.text_size = 20
-        self.button_color = (93, 0, 255)
-        self.button_color_hover = (174, 127, 255)
-
-
-class Button:
-    def __init__(self, x, y, width, height, outline, settings, text="", action=None):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.text = text
-        self.settings = settings
-        self.action = action
-        self.outline = outline
-
-    def draw(self, screen):
-        if self.outline:
-            pygame.draw.rect(screen, self.outline, (self.x - 2, self.y - 2, self.width + 4, self.height + 4), 0)
-
-        color = self.settings.button_color if not self.is_over(
-            pygame.mouse.get_pos()) else self.settings.button_color_hover
-        pygame.draw.rect(screen, color, (self.x, self.y, self.width, self.height), 0)
-
-        if self.text != "":
-            pygame.font.init()
-            font = pygame.font.SysFont('segoeuisemibold', self.settings.text_size)
-            text = font.render(self.text, True, (0, 0, 0))
-            screen.blit(
-                text,
-                (
-                    self.x + (self.width / 2 - text.get_width() / 2),
-                    self.y + (self.height / 2 - text.get_height() / 2)
-                )
-            )
-
-    def update(self, events):
-        for event in events:
-            if event.type == pygame.MOUSEBUTTONDOWN \
-                    and self.is_over(pygame.mouse.get_pos()) \
-                    and self.action:
-                self.action()
-                return True
-        return False
-
-    def is_over(self, pos):
-        if self.x < pos[0] < self.x + self.width:
-            if self.y < pos[1] < self.y + self.height:
-                return True
-        return False
-
-
 in_game: bool = False
-
 
 def play():
     global in_game
@@ -93,7 +40,7 @@ def options_menu():
         Button(80, 530, 200, 50, (0, 0, 0), settings, "Играть!", play),
         Button(300, 530, 200, 50, (0, 0, 0), settings, "Таблица Рекордов", store_score_menu),
         Button(520, 530, 200, 50, (0, 0, 0), settings, "Выйти из игры", exit_game),
-        Button(740, 530, 200, 50, (0, 0, 0), settings, "Настройки",            ),
+        Button(740, 530, 200, 50, (0, 0, 0), settings, "Настройки", settings_menu),
     ]
     background_file = "./textures/bg/pacman2.jpg"
 
---
+++ settings_menu.py
@@ -0,0 +1,54 @@
+from typing import Tuple
+from globalclasses import Button
+from globalclasses import GuiSettings
+import globalvars
+
+import pygame
+
+def settings_menu():
+    main_screen = pygame.display.set_mode((1066, 600))
+    running = True
+    settings = GuiSettings()
+    clock = pygame.time.Clock()
+    buttons = [
+        Button(340, 80, 200, 50, (0, 0, 0), settings, "Лёгкая", ),
+        Button(560, 80, 200, 50, (0, 0, 0), settings, "Нормальная", ),
+        Button(780, 80, 200, 50, (0, 0, 0), settings, "Сложная", ),
+
+
+        Button(340, 180, 200, 50, (0, 0, 0), settings, "Обычные", ),
+        Button(780, 180, 200, 50, (0, 0, 0), settings, "Альтернативные", ),
+    ]
+    text_font = pygame.font.SysFont("Consolas", 32)
+    back_color = "black"
+    # noinspection PyTypeChecker
+    text_color: Tuple[int, int, int] = "white"
+    while running:
+        events = pygame.event.get()
+        if pygame.event.peek(pump=True):
+            main_screen.fill(back_color)
+            main_screen.blit(
+                text_font.render("Настройки", False, text_color),
+                (
+                    main_screen.get_width() // 2 - text_font.size("Настройки")[0] // 2,
+                    20
+                )
+            )
+
+            main_screen.blit(text_font.render("Сложность", False, text_color), (20, 90))
+            main_screen.blit(text_font.render("Текстуры", False, text_color), (20, 190))
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        running = False
+
+            for button in buttons:
+                button.update(events)
+
+            for button in buttons:
+                button.draw(main_screen)
+
+            pygame.display.flip()
